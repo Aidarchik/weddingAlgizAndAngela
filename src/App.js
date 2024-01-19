07831daea7effb8af1aa98a7d6c5f3de2fafefe5
Aidarchik <ishimgrad@gmail.com>
2024-01-19 16:31:53 +0300

    Наполнениен

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import homeWat from './assets/316-3164413_home-wat.png';
 import './App.css';
+import Tmp from './components/tmp'
 
 function App() {
   return (
     <div className="App">
-      <div className='h-screen mx-auto bg-hero-pattern bg-scroll bg-cover bg-center'>
-        <div className='container mx-auto flex'>
-          <img src={homeWat} className='relative top-[calc(-6vw)] ' alt=""></img>
+      <div className='h-screen mx-auto bg-hero-pattern bg-fixed bg-cover bg-center'>
+        <div className='container mx-auto px-2'>
+        <img src={homeWat} className='relative top-[calc(-6vw)]' alt=""></img>
+          <Tmp className=''/>
         </div>
       </div>
     </div>
