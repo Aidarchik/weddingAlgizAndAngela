5c846ef24d1ac798424b7d85a75e31c2bcf60b8c
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2024-01-19 19:26:12 +0500

    Opacity

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <div className='h-screen mx-auto bg-hero-pattern bg-fixed bg-cover bg-center'>
         <div className='container mx-auto px-2'>
-        <img src={homeWat} className='relative top-[calc(-6vw)]' alt=""></img>
+        <img src={homeWat} className='relative opacity-25 top-[calc(-6vw)]' alt=""></img>
           <Tmp className=''/>
         </div>
       </div>
