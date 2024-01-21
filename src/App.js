c20d0fc8c6023cbda8b71c2b1acc477f82e41e6a
Aidarchik <ishimgrad@gmail.com>
2024-01-22 01:18:00 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -12,39 +12,37 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="mx-auto bg-hero-pattern bg-fixed bg-cover bg-center">
-        <div className=" border-2 h-[720px] border-gray-950">
-          <img
-            src={greenBranch}
-            className="absolute top-[-24px] w-[439px] left-[calc(50%-160px-59px)]"
-            alt=""
-          ></img>
-          <img
-            src={invite}
-            className="absolute top-28 sm:left-[calc(50%-160px+84px)] left-[calc(50%-600px+531px)] w-50 h-20 contrast-[10%]"
-            alt=""
-          ></img>
-          <img
-            src={algiz}
-            className="absolute top-[274px] left-[calc(50%-160px-16px)] w-44"
-            alt=""
-          ></img>
-          <img
-            src={and}
-            className="absolute top-[258px] left-[calc(50%-160px+72px)] opacity-20 w-44 contrast-[10%]"
-            alt=""
-          ></img>
-          <img
-            src={angela}
-            className="absolute top-[360px] left-[calc(50%-160px+110px)] w-44"
-            alt=""
-          ></img>
-          <img
-            src={egg}
-            className="absolute top-[526px] left-[calc(50%-160px+57px)] w-52"
-            alt=""
-          ></img>
-        </div>
+      <div className="mx-auto bg-hero-pattern bg-fixed bg-cover bg-center h-screen">
+        <img
+          src={greenBranch}
+          className="absolute top-[-24px] w-[439px] left-[calc(50%-160px-59px)]"
+          alt=""
+        ></img>
+        <img
+          src={invite}
+          className="absolute top-28 sm:left-[calc(50%-160px+84px)] left-[calc(50%-600px+531px)] w-50 h-20 contrast-[10%]"
+          alt=""
+        ></img>
+        <img
+          src={algiz}
+          className="absolute top-[274px] left-[calc(50%-160px-16px)] w-44"
+          alt=""
+        ></img>
+        <img
+          src={and}
+          className="absolute top-[258px] left-[calc(50%-160px+72px)] opacity-20 w-44 contrast-[10%]"
+          alt=""
+        ></img>
+        <img
+          src={angela}
+          className="absolute top-[360px] left-[calc(50%-160px+110px)] w-44"
+          alt=""
+        ></img>
+        <img
+          src={egg}
+          className="absolute top-[526px] left-[calc(50%-160px+57px)] w-52"
+          alt=""
+        ></img>
       </div>
     </div>
   );
