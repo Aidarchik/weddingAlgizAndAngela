a3bab04e5857b01e5229dd681ea70a739672feef
Aidarchik <ishimgrad@gmail.com>
2024-01-23 23:17:09 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
     <div className="App">
       <div className="bg-hero-pattern bg-fixed bg-cover bg-center">
         <Invite image={{ greenBranch, invite, algiz, and, angela, egg }} />
-        <Invite image={{ greenBranch, invite, algiz, and, angela, egg }} />
         {/* <div className="h-screen border-2 border-red-800 flex justify-center">
           <div className="relative w-20 sm:w-40 md:w-60 h-20 border-2 border-green-800">
             <div className="absolute top-[-20px] left-[calc(50%-160px+140px)] sm:left-[calc(50%-160px+97px)] w-10 sm:w-32 md:w-52 h-10 border-2 border-blue-800"></div>
