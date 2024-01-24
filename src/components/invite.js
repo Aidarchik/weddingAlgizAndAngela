194276024e779bd744987f5d320b59773de9570b
Aidarchik <ishimgrad@gmail.com>
2024-01-24 21:04:46 +0500

    адаптив

--- src/components/invite.js
+++ src/components/invite.js
@@ -1,23 +1,19 @@
 export default function Invite(props) {
   const { greenBranch, invite, algiz, and, angela, egg } = props.image;
   return (
-    <div className="h-full min-h-screen border-2 border-red-800 flex container mx-auto">
+    <div className="h-full min-h-screen border-2 container mx-auto border-red-800 overflow-hidden mb-5">
       <div className="invite__row flex flex-col content-center justify-start">
         <div className="invite__column flex-grow-0 flex-shrink basis-auto">
           <div className="invite__item border-0 border-blue-700">
             <div className="invite__item__row flex justify-center">
               <div className="invite__item__image flex-grow-0 flex-shrink-0 basis-[120%]">
-                <img
-                  src={greenBranch}
-                  alt=""
-                  className="relative top-[-7vw] mx-auto"
-                />
+                <img src={greenBranch} alt="" className="relative mx-auto" />
               </div>
             </div>
           </div>
         </div>
         <div className="invite__column flex-grow-0 flex-shrink basis-auto">
-          <div className="invite__item border-0 border-blue-700 mb-6">
+          <div className="invite__item border-0 border-blue-700">
             <div className="invite__item__row flex justify-center">
               <div className="invite__item__image flex-grow-0 flex-shrink-0 basis-[43%]">
                 <img
@@ -30,14 +26,17 @@ export default function Invite(props) {
           </div>
         </div>
         <div className="invite__column flex-grow-0 flex-shrink basis-auto">
-          <div className="invite__item border-0 border-blue-700">
+          <div className="invite__item">
             <div className="invite__item__row flex justify-center">
-              <div className="invite__item__image flex-grow-0 flex-shrink-0 basis-[50%]">
-                <img
-                  src={and}
-                  alt=""
-                  className="opacity-20 contrast-[10%] mx-auto"
-                />
+              <div className="invite__item__image flex-grow-0 flex-shrink-0 basis-[60%] border-2 border-blue-700 bg-bg-and bg-local bg-center">
+                <div className="flex flex-col names__row border-2 border-blue-700">
+                  <div className="names__images mr-[50%]">
+                    <img src={algiz} alt="" />
+                  </div>
+                  <div className="names__images ml-[40%]">
+                    <img src={angela} alt="" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      backgroundImage:{
-        'hero-pattern':"url('./assets/background.png')",
-      }
+      backgroundImage: {
+        "hero-pattern": "url('./assets/background.png')",
+        "bg-and": "url('./assets/and.png')",
+      },
     },
   },
   plugins: [],
-}
-
+};
