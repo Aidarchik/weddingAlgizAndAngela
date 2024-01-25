d78d429141f5a4ee2186412f48d3e85afe94f4f6
Aidarchik <ishimgrad@gmail.com>
2024-01-25 22:23:57 +0500

    invite ready

--- src/App.js
+++ src/App.js
@@ -1,69 +1,11 @@
-import greenBranch from "./assets/green_branch.png";
-import invite from "./assets/invite.png";
-import algiz from "./assets/algiz.png";
-import algizBlack from "./assets/algiz_black.png";
-import and from "./assets/and.png";
-import angela from "./assets/angela.png";
-import angelaBlack from "./assets/angela_black.png";
-import egg from "./assets/pngegg.png";
 import "./App.css";
 import Invite from "./components/invite/invite";
-import { useInView } from "react-intersection-observer";
-import { useEffect, useState } from "react";
 // import Tmp from './components/tmp'
 
 function App() {
-  const { ref, inView } = useInView({
-    threshold: 0,
-    triggerOnce: false,
-  });
-
-  const [flag, setFlag] = useState(false);
-
-  useEffect(() => {
-    const interval = setInterval(() => setFlag((prev) => !prev), 1000);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div className="App">
-      <div
-        className={`${
-          !inView && "bg-hero-pattern"
-        } bg-fixed bg-cover bg-center relative z-50`}
-      >
-        <Invite
-          image={{
-            greenBranch,
-            invite,
-            algiz,
-            algizBlack,
-            and,
-            angela,
-            angelaBlack,
-            egg,
-            flag,
-          }}
-        />
-        <Invite
-          image={{
-            greenBranch,
-            invite,
-            algiz,
-            algizBlack,
-            and,
-            angela,
-            angelaBlack,
-            egg,
-            inView,
-          }}
-        />
-        {/* <div className="h-screen border-2 border-red-800 flex justify-center">
-          <div className="relative w-20 sm:w-40 md:w-60 h-20 border-2 border-green-800">
-            <div className="absolute top-[-20px] left-[calc(50%-160px+140px)] sm:left-[calc(50%-160px+97px)] w-10 sm:w-32 md:w-52 h-10 border-2 border-blue-800"></div>
-          </div>
-        </div> */}
-      </div>
+      <Invite />
     </div>
   );
 }
--- src/components/invite/invite.js
+++ src/components/invite/invite.js
@@ -1,24 +1,41 @@
+import greenBranch from "../../assets/green_branch.png";
+import invite from "../../assets/invite.png";
+import algiz from "../../assets/algiz.png";
+import algizBlack from "../../assets/algiz_black.png";
+import and from "../../assets/and.png";
+import angela from "../../assets/angela.png";
+import angelaBlack from "../../assets/angela_black.png";
+import egg from "../../assets/pngegg.png";
 import "./invite.css";
-export default function Invite(props) {
-  const {
-    greenBranch,
-    invite,
-    algiz,
-    algizBlack,
-    and,
-    angela,
-    angelaBlack,
-    egg,
-    flag,
-  } = props.image;
+import { useEffect, useState } from "react";
+
+export default function Invite() {
+  const [domReady, setDomReady] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => setDomReady((prev) => !prev), 3000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="h-full min-h-screen border-0 container mx-auto border-red-800 overflow-hidden pb-5">
+      <div
+        className={`absolute z-40 top-0 left-0 bg-white w-full h-full min-h-screen trasition duration-[1000ms] ${
+          domReady ? "opacity-0" : "opacity-100"
+        }`}
+      ></div>
       <div className="invite__row flex flex-col content-center justify-start lg:relative">
         <div className="invite__column flex-grow-0 flex-shrink basis-auto">
           <div className="invite__item border-0 border-blue-700">
             <div className="invite__item__row flex justify-center">
               <div className="invite__item__image flex-grow-0 flex-shrink-0 basis-[120%] lg:basis-[60%]">
-                <img src={greenBranch} alt="" className="relative mx-auto" />
+                <img
+                  src={greenBranch}
+                  alt=""
+                  className={`relative mx-auto trasition duration-[1000ms] ${
+                    domReady ? "translate-y-0" : "-translate-y-full"
+                  }`}
+                />
               </div>
             </div>
           </div>
@@ -30,7 +47,9 @@ export default function Invite(props) {
                 <img
                   src={invite}
                   alt=""
-                  className="h-fit contrast-[10%] mx-auto"
+                  className={`h-fit contrast-[10%] mx-auto trasition duration-[1000ms] ${
+                    !domReady ? "scale-0" : "-scale-full"
+                  }`}
                 />
               </div>
             </div>
@@ -44,20 +63,34 @@ export default function Invite(props) {
                   <div className="names__column flex">
                     <div className="names__item flex-grow-0 flex-shrink-0 basis-[60%] lg:basis-[60%] mr-[40%] relative">
                       <img src={algiz} alt="" className="algiz" />
-                      <img src={algizBlack} alt="" className="algiz-black" />
+                      <img
+                        src={algizBlack}
+                        alt=""
+                        className={`algiz-black trasition duration-[1500ms] ${
+                          domReady ? "opacity-0" : "opacity-100"
+                        }`}
+                      />
                     </div>
                   </div>
                   <div className="names__column flex">
                     <div className="names__item flex-grow-0 flex-shrink-0 basis-[60%] lg:basis-[60%] ml-[40%] relative">
                       <img src={angela} alt="" className="angela" />
-                      <img src={angelaBlack} alt="" className="angela-black" />
+                      <img
+                        src={angelaBlack}
+                        alt=""
+                        className={`angela-black trasition duration-[1500ms] ${
+                          domReady ? "opacity-0" : "opacity-100"
+                        }`}
+                      />
                     </div>
                   </div>
                   <div className="names__column flex absolute z-0 top-[-5%] left-[30%] md:top-[-5%] md:left-[26%] lg:top-[-10%] lg:left-[25%] xl:top-[5%] xl:left-[30%] 2xl:top-[10%] 2xl:left-[30%]">
                     <div className="names__item flex-grow-0 flex-shrink-0 basis-[75%] lg:basis-full">
                       <img
                         src={and}
-                        className="opacity-20 contrast-[10%]"
+                        className={`opacity-20 contrast-[10%] trasition duration-[1000ms] ${
+                          !domReady ? "scale-0" : "-scale-full"
+                        }`}
                         alt=""
                       />
                     </div>
@@ -71,7 +104,13 @@ export default function Invite(props) {
           <div className="invite__item border-0 border-blue-700">
             <div className="invite__item__row flex justify-center">
               <div className="invite__item__image flex-grow-0 flex-shrink-0 basis-[43%] md:basis-[30%] lg:absolute lg:w-[20%] lg:left-[10%] lg:top-[60%] lg:-z-10 lg:opacity-60">
-                <img src={egg} alt="" className="mx-auto" />
+                <img
+                  src={egg}
+                  alt=""
+                  className={`mx-auto trasition duration-[1000ms] ${
+                    !domReady ? "scale-0" : "-scale-full"
+                  }`}
+                />
               </div>
             </div>
           </div>
